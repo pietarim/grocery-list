0556ecc4112022fd48c666bf51f936c544d893d6
unknown <pietari.m@hotmail.com>
2024-01-09 19:46:36 +0200

    rename client

--- client/src/hooks/useNotofication.ts
+++ client/src/hooks/useNotofication.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { NotificationContext } from '../context/NotificationContext1';
+import { NotificationContext } from '../context/NotificationContext';
 
 export const useNotification = () => {
   const notification = useContext(NotificationContext);
