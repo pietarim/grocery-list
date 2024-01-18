49bd012e694d839a6d2ec03bcce269a31d0baa76
unknown <pietari.m@hotmail.com>
2024-01-18 19:05:07 +0200

    check if client/.env.production wasn't accessed in github actions

--- client/src/config.ts
+++ client/src/config.ts
@@ -12,5 +12,7 @@ const prodConfig: IConfig = {
 
 export const base_url = import.meta.env.VITE_API_URI || 'http://localhost:3001';
 
+/* export const base_url = import.meta.env.VITE_API_URI || 'http://localhost:3001'; */
+
 /* const API_URI = process.env.NODE_ENV === 'production' ? prodConfig.API_URI : devConfig.API_URI;
 export default API_URI; */
